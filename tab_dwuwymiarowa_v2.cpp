e4e5db9d154efb363b855c7355fbc6e5aecd8bc5
Dominik Migacz <dominikmig04@gmail.com>
2025-01-12 10:35:17 +0100

    Aktualizacja pliku tab_dwuwymiarowa_v2.cpp

--- tab_dwuwymiarowa_v2.cpp
+++ tab_dwuwymiarowa_v2.cpp
@@ -1,43 +1,38 @@
 #include <iostream>
-#include <ctime>
 
 using namespace std;
 
-int main() {
+int r1, r2;
 
+int main()
+{
 	srand(time(NULL));
-	int r1, r2, a, b, c;
-	cout << "Podaj rozmiar tablicy dwuwymiarowej. \nPodaj ilosc kolumn Matrixa: ";
+	cout << "Podaj wymiary Matrixa. Podaj y: ";
 	cin >> r1;
-	cout << "Podaj ilosc wierszy Matrixa: ";
+	cout << "Podaj x: ";
 	cin >> r2;
-	cout << "Podaj zakres liczb losowych. \nPodaj dolna granice: ";
-	cin >> a;
-	cout << "Podaj gorna granice: "; 
-	cin >> b;
-	c = b - a;
-	int** tab = new int * [r1];
+
+	int** tab = new int* [r1];
 	for (int i = 0; i < r1; i++)
 	{
 		tab[i] = new int[r2];
 	}
-	int w;
 	for (int i = 0; i < r1; i++)
 	{
 		for (int j = 0; j < r2; j++)
 		{
-			w = rand() % c + a;
-			tab[i][j] = w;
+			tab[i][j]=rand() % 100;
 		}
 	}
 	for (int i = 0; i < r1; i++)
 	{
 		for (int j = 0; j < r2; j++)
 		{
-			cout << tab[i][j] << " ";
+			cout<<tab[i][j]<<" ";
 		}
 		cout << endl;
 	}
+
 	for (int i = 0; i < r1; i++)
 	{
 		delete[] tab[i];
